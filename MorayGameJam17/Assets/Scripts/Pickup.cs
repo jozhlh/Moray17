243a8946b6236e8acef52d9429d7571ca3f9ffe7
Stewart L. McCready <stewmcc@gmail.com>
2017-03-17 20:12:54 +0000

    Added Pickups, and inventory.  - Inventory  - Pickup and drop items.

---
+++ MorayGameJam17/Assets/Scripts/Pickup.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class Pickup : MonoBehaviour {
+
+	[SerializeField]
+	string garbageName = "DefaultName";
+
+	[SerializeField]
+	float itemHeightOffsetWhenPickedUp = 1.0f;
+
+	[SerializeField]
+	Canvas interactableCanvas = null;
+
+	Vector3 initialPosition = Vector3.zero;
+	
+	private void Start() {
+		interactableCanvas.enabled = false;
+		initialPosition = transform.position;
+	}
+
+	public float ItemheightOffset() {
+		return itemHeightOffsetWhenPickedUp;
+	}
+
+	public float InitialHeight() {
+		return initialPosition.y;
+	}
+
+	public void ShowPopUp() {		
+		interactableCanvas.enabled = true;
+	}
+
+	public void HidePopUp() {
+		interactableCanvas.enabled = false;
+	}
+
+	public string Name() {
+		return garbageName;
+	}
+}
