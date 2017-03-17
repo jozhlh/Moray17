418f197b616231963f567b6015911ae6dc985795
rizlaking <josh_hale@btinternet.com>
2017-03-17 22:47:59 +0000

    Added Room Control Scripts
    Added system for allocating rooms

---
+++ MorayGameJam17/Assets/Scripts/Item.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+	public enum ItemType { item1, item2, item3 , item4};
+}
---
+++ MorayGameJam17/Assets/Scripts/RoomControl.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomControl : MonoBehaviour 
+{
+	[SerializeField]
+	private int maxRoomsToBreak = 2;
+	[SerializeField]
+	private Item.ItemType correctItem;
+	[SerializeField]
+	private List<ItemResponse> incorrectResponses = null;
+	private RoomManager roomManager = null;
+	private bool roomFixed = true;
+	int roomID;
+
+	public void Initialise(int id)
+	{
+		roomManager = GetComponentInParent<RoomManager>();
+		roomID = id;
+		int iterator = 0;
+		int maxRooms = roomManager.NumberOfRooms();
+		incorrectResponses = new List<ItemResponse>();
+
+		// create a response for each availabe item type (- the correct item)
+		for (int i = 0; i < System.Enum.GetValues(typeof(Item.ItemType)).Length; i++)
+		{
+			incorrectResponses.Add(new ItemResponse());
+		}
+
+		for (int response = 0; response < incorrectResponses.Count; response++)
+		{
+			// randomly select how many rooms will be broken by presenting the incorrect item
+			incorrectResponses[response].Initialise(maxRoomsToBreak);
+
+			// for each room that will be broken
+			for (int room = 0; room < incorrectResponses[response].roomsToBreak.Length; room++)
+			{
+				// select a room which is not this room
+				bool validRoom = false;
+				while (!validRoom)
+				{
+					int roomToBreak = Random.Range(0, maxRooms);
+					if (roomToBreak != roomID)
+					{
+						incorrectResponses[response].roomsToBreak[room] = roomToBreak;
+						validRoom = true;
+					}
+				}
+			}
+			// move to the next item type
+			iterator++;
+		}
+	}
+
+	public void CheckItem(Item.ItemType presentedItem)
+	{
+		if (presentedItem == correctItem)
+		{
+			// success the room is fixed
+			roomFixed = true;
+		}
+		else
+		{
+			// get a list of rooms to break and break them
+			int[] roomsToBreak = incorrectResponses[(int)presentedItem].roomsToBreak;
+			foreach (int roomNum in roomsToBreak)
+			{
+				roomManager.BreakRoom(roomNum);
+			}
+		}
+	}
+
+	public void Break()
+	{
+		roomFixed = false;
+	}
+}
